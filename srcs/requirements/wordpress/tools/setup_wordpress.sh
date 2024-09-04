84752a8e3eacf11c8c5227a6557915e9533b2a53
malancar <malancar@malancar.malancar.virtualbox.org>
2024-09-04 17:59:26 +0200

    c'est super youpi wordpress

--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -1,58 +1,44 @@
-#!/bin/bash
 
 # sleep 10
-
-# cd /var/www/html/wordpress
+cd /var/www/html/wordpress
 # if ! wp-cli.phar core is-installed; then
 wp-cli.phar cli update  --yes \
 						--allow-root && echo update success 1
 
-wp-cli.phar core download --allow-root
-
-# echo 	QWJROQWEJHROQWEJRQWEJRQWOERJWQEO $SQL_DATABASE \
-# 	$SQL_USER \
-# 	$SQL_PASSWORD \
-# 	$SQL_HOSTNAME 
+# wp-cli.phar core download --allow-root
 
 wp-cli.phar config create	--allow-root \
 					--dbname=$SQL_DATABASE \
 					--dbuser=$SQL_USER \
 					--dbpass=$SQL_PASSWORD \
 					--dbhost=$SQL_HOSTNAME \
-					--url=https://${DOMAIN_NAME} \
-					--extra-php << PHP
-define('WP_DEBUG', true);
-define('WP_DEBUG_LOG', true);
-PHP
-					#--path='/var/www/html/wordpress'
-					# --config-file=/var/www/html/wordpress
+					--url=https://${DOMAIN_NAME}
 
-/usr/local/bin/wp-cli.phar core install	--allow-root \
+wp-cli.phar core install	--allow-root \
 			--url=https://${DOMAIN_NAME} \
 			--title=${SITE_TITLE} \
 			--admin_user=${ADMIN_USER} \
 			--admin_password=${ADMIN_PASSWORD} \
 			--admin_email=${ADMIN_EMAIL};
 
-/usr/local/bin/wp-cli.phar config path
-
-/usr/local/bin/wp-cli.phar user create		--allow-root \
+wp-cli.phar user create		--allow-root \
 			${USER1_LOGIN} ${USER1_MAIL} \
 			--role=author \
 			--user_pass=${USER1_PASS} ;
 
 
-# /usr/local/bin/wp-cli.phar cache flush --allow-root
+# wp-cli.phar cache flush --allow-root
 
-# /usr/local/bin/wp-cli.phar plugin install contact-form-7 --activate
+# wp-cli.phar plugin install contact-form-7 --activate
 
-# /usr/local/bin/wp-cli.phar language core install en_US --activate
+# wp-cli.phar language core install en_US --activate
 
-# /usr/local/bin/wp-cli.phar theme delete twentynineteen twentytwenty
-# /usr/local/bin/wp-cli.phar plugin delete hello
+wp-cli.phar theme install twentytwentytwo --activate --allow-root
+# wp-cli.phar plugin delete hello
 
-# /usr/local/bin/wp-cli.phar rewrite structure '/%postname%/'
+# wp-cli.phar rewrite structure '/%postname%/'
 ls /var/www/html/wordpress
+chow -R www-data:www-data /var/www/html/*
 # fi
 
 if [ ! -d /run/php ]; then
